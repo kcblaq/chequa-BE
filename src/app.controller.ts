07365357de7b0cc3408d1bc010c3b38b422af5fa
kcblack22@gmail.com <kcblack22@gmail.com>
2023-03-18 17:43:46 +0100

    setup

---
+++ src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  welcome(): any {
+    throw new Error('Method not implemented.');
+  }
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.welcome();
+  }
+}
